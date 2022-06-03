6e4124252ea6af34fba729a05a9e8bf44b49510c
NetFlex <amvset@gmail.com>
2022-06-03 04:27:31 +0000

    Firts Realese

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import subprocess as sp
+
+app = Flask(__name__, static_folder='assets', static_url_path='')
+
+# Pass the required route to the decorator.
+@app.route("/growtopia/server_data.php")
+def hello():
+    return app.send_static_file('server-data.html')
+	
+@app.route("/")
+def index():
+	return app.send_static_file('redir.html')
+
+if __name__ == "__main__":
+	app.run(debug=True, port=3000)
