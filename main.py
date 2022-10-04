3d97988d368b929df1e064ed67f87328ba763c08
netflexs <112264339+netflexs@users.noreply.github.com>
2022-10-04 12:35:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import subprocess as sp
 app = Flask(__name__, static_folder='assets', static_url_path='')
 
 # Pass the required route to the decorator.
-@app.route("/growtopia/server_data.php")
+@app.route("/growtopia/server_data.php", methods = ['POST'])
 def hello():
     return app.send_static_file('server-data.html')
 	
